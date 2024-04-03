1f5f72a706ee9b617235880d071e316b894ecf91
matt a <55745472+Matthew-Ashford@users.noreply.github.com>
2024-04-02 20:46:06 -0400

    reorganize

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/nav/NavBar";
+import SliderBar from "./components/slider/SliderBar";
 
 export default function Home() {
   return (
 	<div>
 		<NavBar />
+		<SliderBar />
 	</div>
   );
 }
