56375e9b879b1add9adc3f26fefd95dc1a0aba4c
matt a <55745472+4bash@users.noreply.github.com>
2024-04-06 14:11:02 -0400

    working carousel

--- src/app/components/carousel/Carousel.js
+++ src/app/components/carousel/Carousel.js
@@ -1,55 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import "./Carousel.css"
 
 const carousel = props => {
 
-	const [carouselPosition, updateCarouselPosition] = useState(-1);
+	//Notes
+	//There are three divs that are used to change the carousel position, with IDs 0, 1, 2
+	//When a user clicks one of the divs, the stateTarget moves to the divs ID * 100
+	//Via useEffect, carouselPosition will update 1 number at a time till it reaches stateTarget
+
+	const [carouselPosition, updateCarouselPosition] = useState(0);
+	const [stateTarget, updateStateTarget] = useState(0);
 
 	const data = ["/slider_beigeDunks.png", "/slider_grayDunks.png", "/slider_lilacDunks.png"];
 
+	const divOnClick = event => {
+
+		let num = Number(event.target.id);
+
+		if(num == carouselPosition) {
+			return;
+		}
+
+		num = (num == data.length) ? 0:num * 100;
+
+		updateStateTarget(num);
+	}
+
+	useEffect(() => {
+		//Called whenever the componenet rerenders
+
+		if(carouselPosition == stateTarget)
+		{
+			return;
+		}
+
+		var timer = setTimeout(() => {
+			updateCarouselPosition(prevState => prevState + (stateTarget > carouselPosition ? 1:-1));
+		}, 1.2);
+
+		return () => {
+			//Also called whenever the component rerenders, cannot clear the timeout unless the "animation" is done
+			if(carouselPosition != stateTarget)
+			{
+				return;
+			}
+
+			clearTimeout(timer);
+		}
+	 },[carouselPosition, stateTarget])
+
 	return (
 	<div className="carousel">
-		<div className="carousel_Images"></div>
+		<div className="carousel_Images">
 		{
-			data.map(element => {
-			<div className="carousel_Item">
-				<img src={element} style={transform:'translate({-$(carouselPosition * 100)%'}/>
-			</div>
-			});
+			data.map((element, idx) => {
+				return (
+					<div key={idx} className="carousel_Item" style={{transform: `translate(-${carouselPosition}%)`}}>
+						<img src={element} key={idx}/>
+					</div>
+				);
+			})
 		}
 		</div>
 		<div className="carousel_Dots">
-			<div className="carousel_dot"></div>
-			<div className="carousel_dot"></div>
-			<div className="carousel_dot"></div>
+			<div className="carousel_dot" id="0" onClick={divOnClick}></div>
+			<div className="carousel_dot" id="1" onClick={divOnClick}></div>
+			<div className="carousel_dot" id="2" onClick={divOnClick}></div>
 		</div>
 	</div>
 	);
-
-
-
-		<div className="carousel">
-			<div className="carousel_Images">
-				<div className="carousel_Item">
-					<img src={"/slider_beigeDunks.png"}/>
-				</div>
-
-				<div className="carousel_Item">
-					<img src={"/slider_grayDunks.png"}/>
-				</div>
-
-				<div className="carousel_Item">
-					<img src={"/slider_lilacDunks.png"}/>
-				</div>
-			</div>
-			<div className="carousel_Dots">
-				<div className="carousel_dot"></div>
-				<div className="carousel_dot"></div>
-				<div className="carousel_dot"></div>
-			</div>
-		</div>
-	)
 }
 
 export default carousel;
