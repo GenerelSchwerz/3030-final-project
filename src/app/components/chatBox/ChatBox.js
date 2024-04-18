feeab7574ade8d254d0bf6adf60b8f653a5ef435
matt a <55745472+4bash@users.noreply.github.com>
2024-04-17 21:02:55 -0400

    temp

--- src/app/components/chatBox/ChatBox.js
+++ src/app/components/chatBox/ChatBox.js
@@ -1,8 +1,9 @@
 import "./ChatBox.css"
+import { useState } from "react";
 
 export default function ChatBox() {
 
-	const data = [
+	const [data, updateDatat = useState([
 		{
 			text:"textttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt",
 			inbound: true
@@ -19,24 +20,43 @@ export default function ChatBox() {
 			text:"texttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt",
 			inbound: false
 		}
-	];
+	]);
+
+	const [input, updateInput] = useState("");
+
+	const onInputChange = event => {
+		updateInput(event.target.value);
+	}
+
+	const onSubmitSend = event => {
+		console.log(data.length);
+		let x = data.push(
+			{
+				text: input,
+				inbound: false
+			}
+		);
+		console.log(data);
+		updateInput("");
+	}
 
 	return (
 		<div className="chatbox_container">
 			<div className="chatbox">
 				<div className="chatbox_heading">
 					<h3>trashford</h3>
+					<input className="chatbox_exitButton" type="button" value={"x"}/>
 				</div>
 				<div className="chatbox_textContainer">
 					{
 						data.map(obj => {
 							return (
 								obj.inbound ? (
-									<div className="chatbox_textElementIn">
+									<div className="chatbox_textElementIn" key={data.indexOf(obj)}>
 										<p>{obj.text}</p>
 									</div>
 								) : (
-									<div className="chatbox_textElementOut">
+									<div className="chatbox_textElementOut" key={data.indexOf(obj)}>
 										<p>{obj.text}</p>
 									</div>
 								)
@@ -45,8 +65,8 @@ export default function ChatBox() {
 					}
 				</div>
 				<div className="chatbox_buttonContainer">
-					<input className="chatbox_searchBar" type="text" placeholder="Message..." />
-					<input className="chatbox_sendButton" type="button" value={"Send"} />
+					<input className="chatbox_searchBar" type="text" placeholder="Message..." onChange={onInputChange} value={input}/>
+					<input className="chatbox_sendButton" type="button" value={"Send"} onClick={onSubmitSend}/>
 				</div>
 			</div>
 		</div>
