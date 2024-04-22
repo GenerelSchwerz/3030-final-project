019a10d09fdebad60f6528a3ad656bed4e73629a
mmm29992 <121207758+mmm29992@users.noreply.github.com>
2024-04-22 18:52:21 -0400

    did more of the individualshoe page

--- src/app/individualshoe/page.js
+++ src/app/individualshoe/page.js
@@ -1,11 +1,11 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import * as api from "../utils";
 import NavBar from "../components/nav/NavBar2";
 import Carousel2 from "../components/carousel/Carousel2";
 import { AuthProvider } from "../context";
+import ".//Individualshoebttmcomponent.css";
 
 export default function Individualshoepage({ searchParams }) {
   const [shoeData, setShoeData] = useState(null);
@@ -24,17 +24,34 @@ export default function Individualshoepage({ searchParams }) {
   return shoeData == null ? (
     <div>Loading...</div>
   ) : (
-	<>
-	<AuthProvider>
-      <NavBar />
-      <Carousel2 />
-      <h1>Shoe ID: {shoeData.id}</h1>
-      <h1>Shoe Name: {shoeData.name}</h1>
-      <h1>Shoe Price: {shoeData.price}</h1>
-      <h1>Shoe Description: {shoeData.description}</h1>
-      <h1>Shoe Model: {shoeData.model}</h1>
-      <img src={shoeData.src} width={300} height={"auto"} alt="image"></img>
-	</AuthProvider>
-	</>
+    <>
+      <AuthProvider>
+        <NavBar />
+        <Carousel2 />
+        <div className="individualshoecontainer">
+          <hr />
+          <div className="grayarea">
+            <img src="/backarrow.svg" />
+            <h1> {shoeData.name}</h1>
+            <div className="bottomarea">
+              <div className="bottomleftarea">
+                <div className="center">
+                  {" "}
+                  <div className="centerimagecontainer">
+                  </div>
+                </div>
+              </div>
+              <div className="bottomrightarea">hi</div>{" "}
+            </div>
+          </div>
+        </div>
+        <h1>Shoe ID: {shoeData.id}</h1>
+        <h1>Shoe Name: {shoeData.name}</h1>
+        <h1>Shoe Price: {shoeData.price}</h1>
+        <h1>Shoe Description: {shoeData.description}</h1>
+        <h1>Shoe Model: {shoeData.model}</h1>
+        <img src={shoeData.src} width={300} height={"auto"} alt="image"></img>
+      </AuthProvider>
+    </>
   );
 }
