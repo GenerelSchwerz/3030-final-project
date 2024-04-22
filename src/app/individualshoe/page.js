b40a1241c78e37940af48e6957a12a99ae76b67f
matt a <55745472+4bash@users.noreply.github.com>
2024-04-21 23:04:16 -0400

    temp page fix

--- src/app/individualshoe/page.js
+++ src/app/individualshoe/page.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import * as api from "../utils";
 import NavBar from "../components/nav/NavBar2";
 import Carousel2 from "../components/carousel/Carousel2";
+import { AuthProvider } from "../context";
 
 export default function Individualshoepage({ searchParams }) {
   const [shoeData, setShoeData] = useState(null);
@@ -23,7 +24,8 @@ export default function Individualshoepage({ searchParams }) {
   return shoeData == null ? (
     <div>Loading...</div>
   ) : (
-    <div>
+	<>
+	<AuthProvider>
       <NavBar />
       <Carousel2 />
       <h1>Shoe ID: {shoeData.id}</h1>
@@ -32,6 +34,7 @@ export default function Individualshoepage({ searchParams }) {
       <h1>Shoe Description: {shoeData.description}</h1>
       <h1>Shoe Model: {shoeData.model}</h1>
       <img src={shoeData.src} width={300} height={"auto"} alt="image"></img>
-    </div>
+	</AuthProvider>
+	</>
   );
 }
