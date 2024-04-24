b403866938c40bcdb8a864d17971b8c6343ae6d3
GenerelSchwerz <xXSG.KingXx@gmail.com>
2024-04-23 23:57:03 -0400

    new updates

--- src/app/context.js
+++ src/app/context.js
@@ -4,31 +4,44 @@ import { getInfo } from './utils';
 const AuthContext = createContext(); // Creates a Context object.
 
 export const AuthProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(2);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   // Placeholder for login/logout logic
-  const login = (token) => {
-    localStorage.setItem('token', token);
-    setLoggedIn(true);
-  }
-  const logout = () => {
-    localStorage.removeItem('token');
-    setLoggedIn(false);
+  // const login = (token) => {
+  //   localStorage.setItem('token', token);
+  //   setLoggedIn(true);
+  // }
+  // const logout = () => {
+  //   localStorage.removeItem('token');
+  //   setLoggedIn(false);
+  // }
+
+  const setSetLoggedIn = (value, token) => {
+    if (value === true) {
+      if (token === undefined) throw new Error('Token is required');
+      localStorage.setItem('token', token)
+    }
+
+    else if (value === false) localStorage.removeItem('token');
+    setLoggedIn(value);
   }
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      setLoggedIn(2);
+      // due to having token loaded, we can assume user is logged in (cookie should also be loaded.)
+      setLoggedIn(true);
     }
 
     const controller = new AbortController();
     getInfo(controller)
       .then((response) => {
-        setLoggedIn(1);
+        setLoggedIn(true);
+        localStorage.setItem('token', response.token);
       })
       .catch((err) => {
-        setLoggedIn(2);
+        setLoggedIn(false);
+        localStorage.removeItem('token');
       });
 
     return () => controller.abort();
@@ -36,9 +49,8 @@ export const AuthProvider = ({ children }) => {
 
   }, [setLoggedIn]);
 
-  console.log('RENDERING')
   return (
-    <AuthContext.Provider value={{ loggedIn, login, logout, setLoggedIn }}>
+    <AuthContext.Provider value={{ loggedIn, setLoggedIn: setSetLoggedIn }}>
       {children}
     </AuthContext.Provider>
   );
